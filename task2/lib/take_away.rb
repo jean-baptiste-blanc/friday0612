86a6f78a6f919e622b2e47bf5fc8564b7a6bd0f0
Jean-Baptiste Blanc <jbblanc@gmail.com>
2013-12-06 13:08:20 +0000

    started takeaway

---
+++ task2/lib/take_away.rb
@@ -0,0 +1,21 @@
+class TakeAway
+
+	DISHES = { "Burger" => 10,
+					 "French Fries"  => 3,
+					 "Salad"  => 5,
+					 "steack" => 14
+					}
+
+	def dishes
+		DISHES
+	end
+
+	def check_order_price?(order,expected_price)
+
+	end
+
+
+
+
+
+end
---
+++ task2/spec/takeaway_spec.rb
@@ -0,0 +1,25 @@
+require_relative '../lib/take_away'
+describe TakeAway do
+	let(:take_away) {TakeAway.new}
+	let(:order){['Burger','French Fries']}
+
+	it "TakeAway should have a list of dishes" do
+		expect(take_away.dishes).to be_a_kind_of(Hash)
+	end
+
+	it "TakeAway should have a method better inject" do
+		should respond_to(:check_order_price?) 
+	end
+
+	it "check_order_price should take two argument" do 
+		order = []		
+		take_away.should_receive(:check_order_price?).with(order,2)
+ 		take_away.check_order_price?([],2)
+	end
+	
+	it "check_order_price should know the sum of the prices during an order" do
+		expect(take_away.check_order_price?(order,13)).to be_true
+	end
+
+
+end
